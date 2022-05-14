91cafba95c507ca77ab37393d7298d0aef4d1aa6
Lucilaneo Santos <lucilaneosantos@gmail.com>
2022-05-14 18:05:48 -0300

    Adcionando comentarios do projeto no codigo

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-
+#converte binario em decimal
 def binaryToDecimal(n):
     return int(n, 2)
 
 
 if __name__ == '__main__':
     contem = False
+# verificar se o valor do texto é maior de 8 e não contem valores diferentes de 0 e 1
     valor = input('Informe um Valor Binario: ')
     if len(valor) <= 4:
         for teste in valor:
