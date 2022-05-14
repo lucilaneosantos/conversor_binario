a4684395255fd1771890c84143e9cccdca027d02
Lucilaneo Santos <lucilaneosantos@gmail.com>
2022-05-14 20:52:32 -0300

    Foi alterado as mensagens do para usuario e colocado espacamento entre as linhas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#converte binario em decimal
+# converte binario em decimal
 def binaryToDecimal(n):
     return int(n, 2)
 
@@ -6,15 +6,20 @@ def binaryToDecimal(n):
 if __name__ == '__main__':
     contem = False
 # verificar se o valor do texto é maior de 8 e não contem valores diferentes de 0 e 1
+    print()
     valor = input('Informe um Valor Binario: ')
-    if len(valor) <= 4:
+    print()
+    if len(valor) <= 8:
         for teste in valor:
             if (teste != '0') and (teste != '1'):
                 contem = True
 
         if contem:
-            print('Informe um valor 0 e 1')
+            print('Apenas valores 0 e 1')
+            print()
         else:
-            print(f'Decimal desse numero é {binaryToDecimal(valor)}')
+            print(f'Decimal do numero é {binaryToDecimal(valor)}')
+            print()
     else:
         print(f'Só é permitido até 8 digitos, você digitou {len(valor)}')
+        print()
